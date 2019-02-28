28e1e1819f164c1e227fe6ee24070798d79582b5
yaolei313 <yaolei313@gmail.com>
2019-02-28 11:39:45 +0800

    add

--- main/main.go
+++ main/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"go/types"
+	"errors"
+	"time"
 )
 
 type NameAware interface {
@@ -20,15 +23,66 @@ func printName(aware NameAware) {
 	fmt.Println("name is ", aware.GetName())
 }
 
-
-func swap2(a,b *int){
+func swap2(a, b *int) {
 	tmp := *a
 	*a = *b
 	*b = tmp
 }
 
-func swap1(a,b *int){
-	*a,*b = *b,*a
+func swap1(a, b *int) {
+	*a, *b = *b, *a
+}
+
+func printType(obj interface{}){
+	switch i := obj.(type) {
+	case types.Nil:
+		fmt.Printf("obj is %T\n", i)
+	case int:
+		fmt.Println("obj is int")
+	case bool, string:
+		fmt.Println("obj is bool or string")
+	case Student:
+		fmt.Println("obj is Student")
+	case *Student:
+		fmt.Println("obj is Student Pointer")
+	case uintptr:
+		fmt.Println("obj is point")
+	default:
+		fmt.Println("unknown type")
+	}
+}
+
+func Factorial(n uint64)(result uint64) {
+	if n>0 {
+		return n * Factorial(n-1)
+	}
+	return 1
+}
+
+type handler func(s string) int
+
+func (h handler) add(param1 string,param2 int) int{
+	return h(param1) + param2
+}
+
+func divide(a,b int) (int,error) {
+	if b == 0 {
+		return 0,errors.New("b is zero")
+	} else {
+		return a/b,nil
+	}
+}
+
+// defer
+func sdefer1() int{
+	i:=0
+	// 1.defer在return之后被调用，但其参数为实时解析
+	// 2.多个defer后定义的先被执行
+	// 类似finally吧
+	defer fmt.Println("second call",i)
+	i++
+	defer fmt.Println("first call")
+	return i
 }
 
 func main() {
@@ -63,33 +117,62 @@ func main() {
 	map1["Italy"] = "罗马"
 	fmt.Println(map1)
 
+	capital, ok := map1["US"]
+	if ok {
+		fmt.Println("us capital is", capital)
+	} else {
+		fmt.Println("not found in map")
+	}
+	map1["Japan"]="Tokyo"
+	map1["India"]="New delhi"
+	fmt.Println(map1)
+	delete(map1,"France")
+	delete(map1,"abc")
+	fmt.Println("after modify map1:",map1)
+
 	// 数组，值类型，长度固定，下标位置不支持负数
-	var array1 [2] int
-	array1[0]=100
-	array1[1]=200
-	var array2 = [5]int{1,2,3,4,5}
-	var array3 = [...]int{5,6,7,8,9}
-	fmt.Println(array1,"===",array2,"===",array3)
+	var array1 [2]int
+	array1[0] = 100
+	array1[1] = 200
+	var array2 = [5]int{1, 2, 3, 4, 5}
+	var array3 = [...]int{5, 6, 7, 8, 9}
+	fmt.Println(array1, "===", array2, "===", array3)
 
 	// 不设置值的话，默认值为0
-	var array4 [2][3] int
+	var array4 [2][3]int
 	var array5 = [3][3]int{
-		{11,12,13},
-		{14,15,16},
+		{11, 12, 13},
+		{14, 15, 16},
 	}
 	// 只能写一个...
 	var array6 = [...][3]int{
-		{21,22,23},
-		{24,25,26},
+		{21, 22, 23},
+		{24, 25, 26},
 	}
-	fmt.Println(array4,"===",array5,"===",array6)
+	fmt.Println(array4, "===", array5, "===", array6)
 
-	//slice不存储数据，引用类型，可以看做array的section描述
-	var array7 = []int{1,2,3,11,12,13}
+	// 切片var identifier []type
+	// slice不存储数据，引用类型，可以看做array的section描述
+	var array7 = []int{1, 2, 3, 11, 12, 13}
 	var slice1 = array7[1:2]
-	fmt.Println(slice1, "/", reflect.TypeOf(slice1))
-	slice1[0]=998
-	fmt.Println(array7,"/",slice1)
+	fmt.Println(len(array7),slice1, reflect.TypeOf(slice1), cap(slice1), len(slice1))
+	slice1[0] = 998
+	fmt.Println("slice is a reference",array7, slice1)
+
+	var slice2 []int = make([]int,10)
+	fmt.Printf("slice2 content:%v cap=%d len=%d\n",slice2, cap(slice2), len(slice2))
+	slice2 = append(slice2, 1)
+	slice2 = append(slice2, 2,3,4)
+
+	var slice3 = make([]int,5,10)
+	fmt.Printf("slice3 content:%v\n", slice3)
+
+	// 类型转换
+	var float1 = float32(b1)
+	fmt.Println("after convert", float1, reflect.TypeOf(float1))
+	var float2 = 5/3
+	var float3 = float32(5)/float32(3)
+	fmt.Println("除法:",float2,float3)
 
 	// 常量
 	const f1 string = "123"
@@ -107,16 +190,16 @@ func main() {
 	fmt.Printf("ptr is %v %x\n", ptr, ptr) // v格式带0x
 
 	// 指针的数组
-	var ptr2 [6] *int
-	for i:=0;i<6;i++ {
+	var ptr2 [6]*int
+	for i := 0; i < 6; i++ {
 		ptr2[i] = &array7[i]
 	}
-	fmt.Println("[6] *int ",ptr2)
+	fmt.Println("[6] *int ", ptr2)
 
 	// 指针的指针
 	var ptr3 **int
 	ptr3 = &ptr
-	fmt.Println("point:",ptr,ptr3,*ptr3,**ptr3)
+	fmt.Println("point:", ptr, ptr3, *ptr3, **ptr3)
 
 	// 条件语句
 	if f2 < 100 {
@@ -148,11 +231,12 @@ func main() {
 	}
 
 	student1 := Student{"li bai"}
+	student2 := Student{name:"du fu"}
+	fmt.Println("=============================")
 	printName(&student1)
-	/*switch ti := student1.(type) {
-	case Student:
-		fmt.Println("is student")
-	}*/
+	printType(student2)
+	printType(&student2)
+	fmt.Println("acesss struct field:", student1.name)
 
 	var g1, g2, g3 chan int
 	var h1, h2 int
@@ -187,29 +271,73 @@ func main() {
 	}*/
 
 	// for循环的range可以可以遍历slice，map，数组，字符串
+	for idx,val := range array7 {
+		fmt.Println("idx:",idx,"val:",val)
+	}
+
 	for key, value := range map1 {
 		fmt.Println("key:", key, ", value:", value)
 	}
 
+	// 变量字符串
 	for key, value := range b2 {
 		fmt.Println("key:", key, ", value:", value)
 	}
 
-	fmt.Println("before swap c1:",c1,"c2:",c2)
-	swap1(&c1,&c2)
-	fmt.Println("after swap c1:",c1,"c2:",c2)
-	swap2(&c1,&c2)
-	fmt.Println("after swap c1:",c1,"c2:",c2)
-	c1,c2=c2,c1
-	fmt.Println("after swap c1:",c1,"c2:",c2)
+	fmt.Println("before swap c1:", c1, "c2:", c2)
+	swap1(&c1, &c2)
+	fmt.Println("after swap c1:", c1, "c2:", c2)
+	swap2(&c1, &c2)
+	fmt.Println("after swap c1:", c1, "c2:", c2)
+	c1, c2 = c2, c1
+	fmt.Println("after swap c1:", c1, "c2:", c2)
 
 	// for循环的a:=0，与外层的a不是同一个变量，为 for 循环中的局部变量，因此在执行完for循环后，输出a的值仍然为0
 	var a int = 0
 	fmt.Println("for start")
-	for a:=0; a < 10; a++ {
-		fmt.Print(a,"\t")
+	for a := 0; a < 10; a++ {
+		fmt.Print(a, "\t")
 	}
 	fmt.Println()
 	fmt.Println("for end")
 	fmt.Println(a)
+
+	var len_handler handler = func(s string) int {
+		return len(s)
+	}
+	handler(len_handler).add("abc",10);
+
+	// errors.New("")
+
+	// defer学习
+	var r1 int = sdefer1()
+	fmt.Println("return value", r1)
+
+	// go学习
+	go sayHello("hello li bai")
+	sayHello("hello world")
+
+	// channel学习
+	var chan1 chan int
+	fmt.Println("chan1", chan1)
+	chan1 = make(chan int)
+	defer func(param chan int) { close(param)}(chan1)
+	fmt.Println("chan1", chan1)
+
+	// 如果通道不带缓冲，则发送发会阻塞，若带缓冲，则发送阻塞知道被拷贝到缓冲区，若缓冲已满，同样阻塞
+	var chan2 = make(chan int,10)
+	chan2 <- 1
+	chan2 <- 2
+	fmt.Println(<-chan2,<-chan2)
 }
+
+func sayHello(s string){
+	for i:=0;i<5;i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(s)
+	}
+}
+
+
+
+
